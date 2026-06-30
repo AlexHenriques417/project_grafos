f6f0464e93a91f216a49062918286ef2abe95be1
Davi <davinogueiradasilvan@gmail.com>
2026-06-30 07:37:46 -0300

    fix: retaforamento da classe Vertice

--- src/main/java/br/com/unipe/Vertice.java
+++ src/main/java/br/com/unipe/Vertice.java
@@ -1,65 +1,47 @@
 package br.com.unipe;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 public class Vertice {
-
     private String nome;
     private int grau;
-    private int inDegree; // deve ser 0 para não-dirigido
-    private int outDegree;// deve ser 0 para não-dirigido
-    private List<Vertice> adjacencias; //out
-    private List<Vertice> adjacentes; //in
+    private int inDegree;
+    private int outDegree;
+    private List<Vertice> adjacencias; // Saída
+    private List<Vertice> adjacentes;   // Entrada
 
-    public Vertice (String nome) {
+    // Atributos auxiliares para algoritmos
+    public double distanciaDijkstra;
+    public Vertice pai;
+    public boolean visitado;
+    public int nivel; // Para contar "passos" no BFS
+
+    public Vertice(String nome) {
         this.nome = nome;
-        adjacencias = new ArrayList<>();
-        adjacentes = new ArrayList<>();
-    }
-    public void resetaGraus() {
-        grau = inDegree = outDegree = 0;
+        this.adjacencias = new ArrayList<>();
+        this.adjacentes = new ArrayList<>();
     }
 
-    public void resetaAdjacenciasEAdjacentes() {
-        adjacencias.clear();
-        adjacentes.clear();
+    public void resetaAuxiliares() {
+        this.distanciaDijkstra = Double.MAX_VALUE;
+        this.pai = null;
+        this.visitado = false;
+        this.nivel = -1;
     }
 
-    public void aumentaGrau() { // não-dirigido
-        grau++;
-    }
+    public void resetaGraus() { grau = inDegree = outDegree = 0; }
+    public void resetaAdjacenciasEAdjacentes() { adjacencias.clear(); adjacentes.clear(); }
+    public void aumentaGrau() { grau++; }
+    public void aumentaInDegree() { grau++; inDegree++; }
+    public void aumentaOutDegree() { grau++; outDegree++; }
+    public void adicionaAdjacencia(Vertice vertice) { adjacencias.add(vertice); }
+    public void adicionaAdjacente(Vertice vertice) { adjacentes.add(vertice); }
+    
+    public String getNome() { return nome; }
+    public List<Vertice> getAdjacencias() { return adjacencias; }
+    public List<Vertice> getAdjacentes() { return adjacentes; }
 
-    public void aumentaInDegree() {
-        grau++;
-        inDegree++;
-    }
-
-    public void aumentaOutDegree() {
-        grau++;
-        outDegree++;
-    }
-
-    public void adicionaAdjacencia(Vertice vertice) {
-        adjacencias.add(vertice);
-    }
-
-    public void adicionaAdjacente(Vertice vertice) {
-        adjacentes.add(vertice);
-    }
-
-    public String exibeGraus() {
-        return "\n%s: grau %d (%d in | %d out)".formatted(nome, grau, inDegree, outDegree);
-    }
     @Override
-    public String toString() {
-        return nome;
-    }
-
-
+    public String toString() { return nome; }
 }
